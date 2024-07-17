515d63dcc67b8ba7526f4b36471f8b26805f245d
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-17 22:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n = str.length;
+    for (let i = 0; i < n; ++i) {
+        let found = true;
+        for (let j = 0; j < n; ++j) {
+            if (i !== j && str[i] === str[j]) {
+                found = false;
+                break;
+            }
+        }
+      if (found) {
+            return str[i];
+        }
+    }
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
